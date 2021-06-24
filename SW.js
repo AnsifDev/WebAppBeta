8e646eb5692209a33a7754b6e77edfe360c91d29
AnsifDev <84790890+AnsifDev@users.noreply.github.com>
2021-06-24 21:08:02 +0530

    major update 01

--- SW.js
+++ SW.js
@@ -1,3 +1,13 @@
 self.addEventListener("install", function(e){
   console.log("Install finished");
+  e.waitUntil(
+    caches.open("static").then(function(cache){
+      return cache.addAll([
+        "./App/html/MainActivity.html",
+        "./App/js/MainActivity.js",
+        "https://ansifdev.github.io/MyWeb/img512.png",
+        "https://ansifdev.github.io/MyWeb/img64.png"
+      ]);
+    });
+  );
 });
