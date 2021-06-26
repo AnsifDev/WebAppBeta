d3fb6b59d53bbdc1301ed4c995d028ff8c80d86b
AnsifDev <84790890+AnsifDev@users.noreply.github.com>
2021-06-26 16:20:06 +0530

    New features 01

--- SW.js
+++ SW.js
@@ -6,7 +6,9 @@ self.addEventListener("install", function(e){
         "./App/html/MainActivity.html",
         "./App/js/MainActivity.js",
         "https://ansifdev.github.io/MyWeb/img512.png",
-        "https://ansifdev.github.io/MyWeb/img64.png"
+        "https://ansifdev.github.io/MyWeb/img64.png",
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
       ]);
     })
   );
@@ -22,4 +24,4 @@ self.addEventListener("fetch",function(e){
       }
     })
   )
-})
+});
